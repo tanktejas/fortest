695587263af8cf1c67f8fd0a1cbc295737193645
Tejas Tank <tejastank20001@gmail.com>
2023-08-26 18:27:10 +0530

    add

---
+++ a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int mod = 1000000007;
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    }
+}
